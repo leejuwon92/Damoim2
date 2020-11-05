29df49de803258c8acf86ee1957867b275e7f7c0
KOSTA <KOSTA@192.168.0.184>
2020-11-05 16:19:57 +0900

    1105-소은 client

---
+++ src/damoim/controller/ClientController.java
@@ -0,0 +1,161 @@
+package damoim.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import damoim.dto.ClientDTO;
+import damoim.dto.JoinDTO;
+import damoim.dto.PostDTO;
+import damoim.dto.ReplyDTO;
+import damoim.service.ClientService;
+
+public class ClientController implements Controller {
+
+	@Override
+	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) {
+		return null;
+	}
+	/**
+	 * 모임신청하기
+	 * */
+	public ModelAndView clientJoinMoim(HttpServletRequest request,HttpServletResponse response) throws SQLException{
+		System.out.println("clientJoinMoim call..");
+		ModelAndView mv = new ModelAndView();
+		HttpSession session = request.getSession();
+		String postNo = request.getParameter("post_no");
+		int userNo = (int)session.getAttribute("session_userNo");
+		String category = request.getParameter("category_no");
+		JoinDTO dto = new JoinDTO(0, Integer.parseInt(postNo), userNo,Integer.parseInt(category) );
+		int result= ClientService.clientJoinMoim(dto);
+		if(result > 0) {
+			mv.setViewName(request.getContextPath()+"/index.jsp");
+			mv.setRedirect(true);
+		}else throw new SQLException("모임 신청이 실패하였습니다.");
+		return mv;
+	}
+	/**
+	 * 댓글달기
+	 * */
+	public ModelAndView clientInsertReply(HttpServletRequest request,HttpServletResponse response)throws SQLException {
+		System.out.println("clientInsertReply call..");
+		ModelAndView mv = new ModelAndView();
+		HttpSession session = request.getSession();
+		String postNo = request.getParameter("post_no");
+		String replyContent = request.getParameter("reply_content");
+		int userNo = (int)session.getAttribute("session_userNo");
+		ReplyDTO dto = new ReplyDTO(0,Integer.parseInt(postNo), replyContent, 0, userNo, null);
+		int result = ClientService.clientInsertReply(dto);
+		if(result>0) {
+		}else throw new SQLException("댓글이 정상적으로 입력되지 않았습니다.");
+		return mv;
+	}
+	
+	/**
+	 * 댓글삭제하기
+	 * */
+	public ModelAndView clientDeleteReply (HttpServletRequest request,HttpServletResponse response)throws SQLException {
+		System.out.println("clientDeleteReply call..");
+		ModelAndView mv = new ModelAndView();
+		String questionNo = request.getParameter("question_no");
+		int result = ClientService.clientDeleteReply(Integer.parseInt(questionNo));
+		if(result>0) {
+			
+		}else throw new SQLException("댓글이 정상적으로 삭제되지 않았습니다.");
+		return mv;
+	}
+	
+	/**
+	 * 참여자차트만들기
+	 * */
+	public ModelAndView clientSelectChart(HttpServletRequest request,HttpServletResponse response)throws SQLException {
+		System.out.println("clientSelectChart call..");
+		ModelAndView mv = new ModelAndView();
+		HttpSession session = request.getSession();
+		int userNo = (int)session.getAttribute("session_userNo");
+		int [] list = ClientService.clientSelectChart(userNo);
+		if(list!=null) {
+			request.setAttribute("clientchart", list);
+			mv.setRedirect(false);
+			mv.setViewName(request.getContextPath()+"/client/chart.jsp");
+		}else throw new SQLException("신청한 모임이 없습니다");
+		return mv;
+	}
+	
+	/**
+	 * 개설자차트
+	 * */
+	public ModelAndView hostSelectChart(HttpServletRequest request, HttpServletResponse response) throws SQLException{
+		System.out.println("hostSelectChart call..");
+		ModelAndView mv = new ModelAndView();
+		HttpSession session = request.getSession();
+		int userNo = (int)session.getAttribute("session_userNo");
+		int [] list = ClientService.clientSelectChart(userNo);
+		if(list!=null) {
+			request.setAttribute("hostchart", list);
+			mv.setRedirect(false);
+			mv.setViewName(request.getContextPath()+"/client/chart.jsp");
+		}else throw new SQLException("개설한 모임이 없습니다");
+		return mv;
+	}
+	
+	
+	/**
+	 * 마이페이지에서 내가 참여한 모임보기
+	 * */
+	public ModelAndView clientSelectMoimByMe(HttpServletRequest request,HttpServletResponse response)throws SQLException {
+		System.out.println("clientSelectMoimByMe call..");
+		ModelAndView mv = new ModelAndView();
+		HttpSession session = request.getSession();
+		int userNo = (int)session.getAttribute("session_userNo");
+		List<PostDTO> list 	= ClientService.clientSelectMoimByMe(userNo);
+		if(list!=null) {
+			request.setAttribute("list", list);
+			mv.setRedirect(false);
+			mv.setViewName(request.getContextPath()+"/client/MyJoinMoim");
+		}
+		return mv;
+	}
+	
+	/**
+	 * 모임 취소하기
+	 * */
+	public ModelAndView clientCancleMoim (HttpServletRequest request,HttpServletResponse response) throws SQLException{
+		System.out.println("clientCancleMoim call..");
+		ModelAndView mv = new ModelAndView();
+		String postNo = request.getParameter("post_no");
+		HttpSession session = request.getSession();
+		int userNo = (int)session.getAttribute("session_userNo");
+		int result = ClientService.clientCancleMoim(Integer.parseInt(postNo),userNo);
+		if(result >0) {
+			mv.setRedirect(true);
+			mv.setViewName(request.getContextPath()+"/index.jsp");
+		}else throw new SQLException("모임 신청 취소가 실패하였습니다.");
+		return mv;
+	}
+	
+	
+	/**
+	 * 개인정보 수정하기
+	 * */
+	public ModelAndView clientUpdateClientInform (HttpServletRequest request,HttpServletResponse response) throws SQLException{
+		System.out.println("clientUpdateClientInform call..");
+		ModelAndView mv = new ModelAndView();
+		String userPwd = request.getParameter("userPwd");
+		String userName = request.getParameter("userName");
+		String userEmail = request.getParameter("userEmail");
+		String phoneNo = request.getParameter("phoneNo");
+		HttpSession session = request.getSession();
+		int userNo = (int)session.getAttribute("session_userNo");
+		ClientDTO dto = new ClientDTO(userNo, null, userPwd, userName, userEmail, phoneNo);
+		int result = ClientService.clientUpdateClientInform(dto);
+		if(result > 0 ) {
+			mv.setRedirect(true);
+			mv.setViewName(request.getContextPath()+"/client/MyPage_main.jsp");
+		}else throw new SQLException("정보가 정상적으로 수정되지 않았습니다.");
+		return mv;
+	}
+}
---
+++ src/damoim/dao/ClientDAOImpl.java
@@ -0,0 +1,359 @@
+package damoim.dao;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import damoim.dto.ClientDTO;
+import damoim.dto.JoinDTO;
+import damoim.dto.PostDTO;
+import damoim.dto.ReplyDTO;
+import damoim.util.DbUtil;
+
+public class ClientDAOImpl implements ClientDAO {
+
+	@Override
+	public int clientJoinMoim(JoinDTO joinDTO) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = "insert into join_table(join_no,post_no,user_no,category_no) values (join_no.nextval,?,?,?)";
+		int result = 0;
+		try {
+			if(clientJoinCheck(joinDTO.getUserNo(),joinDTO.getPostNo())==false) {
+			con = DbUtil.getConnection();
+			con.setAutoCommit(false);
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, joinDTO.getPostNo());
+			ps.setInt(2, joinDTO.getUserNo());
+			ps.setInt(3, joinDTO.getCategoryNo());
+			result = ps.executeUpdate();
+			int result1 = plusPostCurrentPeople(con, joinDTO.getPostNo());
+			if (result1 == 0) {
+				con.rollback();
+				}
+			}else throw new SQLException("이미 신청중인 모임입니다");
+		} finally {
+			con.commit();
+			DbUtil.dbClose(ps, con);
+		}
+		return result;
+	}
+
+	@Override
+	public int plusPostCurrentPeople(Connection con, int postNo) throws SQLException {
+		PreparedStatement ps = null;
+		String sql = "update post set current_people=current_people+1 where post_no=?";
+		int result = 0;
+		try {
+			con.setAutoCommit(false);
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, postNo);
+			result = ps.executeUpdate();
+		} finally {
+			con.commit();
+			DbUtil.dbClose(ps, null);
+		}
+		return result;
+	}
+
+	@Override
+	public int clientInsertReply(ReplyDTO replyDTO) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = "insert into QnA (question_no,post_no,reply_content,answer_no,user_no,regdate) values(reply_no.nextval,?,?,null,?,sysdate)";
+		int result = 0;
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, replyDTO.getPostNo());
+			ps.setString(2, replyDTO.getReplyContent());
+			ps.setInt(3, replyDTO.getUserNo());
+			result = ps.executeUpdate();
+		} finally {
+			DbUtil.dbClose(ps, con);
+		}
+		return result;
+	}
+
+	@Override
+	public int clientDeleteReply(int questionNo) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = "delete from QnA where question_no = ?";
+		int result = 0;
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, questionNo);
+			result = ps.executeUpdate();
+		} finally {
+			DbUtil.dbClose(ps, con);
+		}
+		return result;
+	}
+
+	@Override
+	public int[] clientSelectChart(int clientNo) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = "select category_no from join_table where user_no=?";
+		ResultSet rs = null;
+		int count1 = 0;
+		int count2 = 0;
+		int count3 = 0;
+		int count4 = 0;
+		int count5 = 0;
+		int count6 = 0;
+		int count7 = 0;
+		int count8 = 0;
+		int count9 = 0;
+		int count10 = 0;
+		int count11 = 0;
+		int[] result = { count1, count2, count3, count4, count5, count6, count7, count8, count9, count10, count11 };
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, clientNo);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				int cateCount = rs.getInt(1);
+				switch (cateCount) {
+				case 1:
+					count1++;
+				case 2:
+					count2++;
+				case 3:
+					count3++;
+				case 4:
+					count4++;
+				case 5:
+					count5++;
+				case 6:
+					count6++;
+				case 7:
+					count7++;
+				case 8:
+					count8++;
+				case 9:
+					count9++;
+				case 10:
+					count10++;
+				case 11:
+					count11++;
+				}
+				result[0] = count1;
+				result[1] = count2;
+				result[2] = count3;
+				result[3] = count4;
+				result[4] = count5;
+				result[5] = count6;
+				result[6] = count7;
+				result[7] = count8;
+				result[8] = count9;
+				result[9] = count10;
+				result[10] = count11;
+			}
+		} finally {
+			DbUtil.dbClose(rs, ps, con);
+		}
+		return result;
+	}
+	
+	
+	@Override
+	public int[] hostSelectChart(int clientNo) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = "select category_no from post where user_no=?";
+		ResultSet rs = null;
+		int count1 = 0;
+		int count2 = 0;
+		int count3 = 0;
+		int count4 = 0;
+		int count5 = 0;
+		int count6 = 0;
+		int count7 = 0;
+		int count8 = 0;
+		int count9 = 0;
+		int count10 = 0;
+		int count11 = 0;
+		int[] result = { count1, count2, count3, count4, count5, count6, count7, count8, count9, count10, count11 };
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, clientNo);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				int cateCount = rs.getInt(1);
+				switch (cateCount) {
+				case 1:
+					count1++;
+				case 2:
+					count2++;
+				case 3:
+					count3++;
+				case 4:
+					count4++;
+				case 5:
+					count5++;
+				case 6:
+					count6++;
+				case 7:
+					count7++;
+				case 8:
+					count8++;
+				case 9:
+					count9++;
+				case 10:
+					count10++;
+				case 11:
+					count11++;
+				}
+				result[0] = count1;
+				result[1] = count2;
+				result[2] = count3;
+				result[3] = count4;
+				result[4] = count5;
+				result[5] = count6;
+				result[6] = count7;
+				result[7] = count8;
+				result[8] = count9;
+				result[9] = count10;
+				result[10] = count11;
+			}
+		} finally {
+			DbUtil.dbClose(rs, ps, con);
+		}
+		return result;
+	}
+	
+	
+
+	@Override
+	public List<PostDTO> clientSelectMoimByMe(int userNo) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String sql = "select * from join_table jo join post po on jo.post_no = po.post_no where jo.user_no=?";
+		List<PostDTO> list = new ArrayList<PostDTO>();
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, userNo);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				int postNo = rs.getInt(1);
+				int dbuserNo = rs.getInt(2);
+				String postTitle = rs.getString(3);
+				String postDescr = rs.getString(4);
+				String postContent = rs.getString(5);
+				int categoryCode = rs.getInt(6);
+				int locationCode = rs.getInt(7);
+				String locationDetail = rs.getString(8);
+				String deadline = rs.getString(9);
+				String meetingDate = rs.getString(10);
+				int totalPeople = rs.getInt(11);
+				int currentPeople = rs.getInt(12);
+				String thumbnailFile = rs.getString(13);
+				String bannerFile = rs.getString(14);
+				list.add(new PostDTO(postNo, dbuserNo, postTitle, postDescr, postContent, categoryCode, locationCode,
+						locationDetail, deadline, meetingDate, totalPeople, currentPeople, thumbnailFile, bannerFile));
+			}
+		} finally {
+			DbUtil.dbClose(rs, ps, con);
+		}
+		return list;
+	}
+
+	@Override
+	public int clientCancleMoim(int postNo, int userNo) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		int result = 0;
+		String sql = "delete from join_table where post_no=? and user_no=?";
+		try {
+			con = DbUtil.getConnection();
+			con.setAutoCommit(false);
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, postNo);
+			ps.setInt(2, userNo);
+			result = ps.executeUpdate();
+			int result1 = minusPostCurrentPeople(con, postNo);
+			if (result1 == 0) {
+				con.rollback();
+			}
+		} finally {
+			con.commit();
+			DbUtil.dbClose(ps, con);
+		}
+		return result;
+	}
+
+	@Override
+	public int minusPostCurrentPeople(Connection con, int postNo) throws SQLException {
+		PreparedStatement ps = null;
+		String sql = "update post set current_people=current_people-1 where post_no=?";
+		int result = 0;
+		try {
+			con.setAutoCommit(false);
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, postNo);
+			result = ps.executeUpdate();
+		} finally {
+			con.commit();
+			DbUtil.dbClose(ps, null);
+		}
+		return result;
+	}
+
+	@Override
+	public int clientUpdateClientInform(ClientDTO clientDTO) throws SQLException {
+		PreparedStatement ps = null;
+		Connection con = null;
+		int result = 0;
+		String sql = "update user_table set user_pwd=?,user_name=?,user_email=?, user_phone=? where user_no=?";
+
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, clientDTO.getUserPwd());
+			ps.setString(2, clientDTO.getUserName());
+			ps.setString(3, clientDTO.getUserEmail());
+			ps.setString(4, clientDTO.getPhoneNo());
+			ps.setInt(5, clientDTO.getUserNo());
+			result = ps.executeUpdate();
+		} finally {
+			DbUtil.dbClose(ps, con);
+		}
+		return result;
+	}
+
+	@Override
+	public boolean clientJoinCheck(int userNo, int postNo) throws SQLException {
+		PreparedStatement ps = null;
+		Connection con = null;
+		ResultSet rs = null;
+		String sql = "select user_no from join_table where user_no=? and post_no=?";
+		boolean result = false;
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, userNo);
+			ps.setInt(2, postNo);
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				result = true;
+			}
+		} finally {
+			DbUtil.dbClose(rs, ps, con);
+		}
+		return result;
+	}
+
+	
+
+}
